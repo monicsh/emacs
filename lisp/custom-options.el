bf460f1301fa6cf9e91c6a93595b865fbd2eaeec
Kshitij Gaipal <ksgaipal@microsoft.com>
2019-12-13 13:16:52 -0800

    new package added deadgrep

--- lisp/custom-options.el
+++ lisp/custom-options.el
@@ -19,13 +19,13 @@
  '(ido-mode (quote buffer) nil (ido))
  '(indent-tabs-mode nil)
  '(initial-buffer-choice nil)
- '(json-reformat:indent-width 2)
+ '(json-reformat:indent-width 2 t)
  '(kill-whole-line t)
  '(make-backup-files nil)
  '(mode-line-in-non-selected-windows t)
  '(package-selected-packages
    (quote
-    (annotate-depth anzu auctex auto-complete buffer-move counsel csharp-mode dynamic-ruler easy-kill-extras edit-at-point fic-mode fold-this highlight-escape-sequences highlight-symbol indent-guide json-mode loccur magit magit-gitflow msvc noccur occur-x omnisharp powershell restart-emacs ripgrep tfs use-package web-mode ws-butler xah-find)))
+    (annotate-depth anzu auctex auto-complete buffer-move counsel csharp-mode deadgrep dynamic-ruler easy-kill-extras edit-at-point fic-mode fold-this highlight-escape-sequences highlight-symbol indent-guide json-mode loccur magit magit-gitflow msvc noccur occur-x omnisharp powershell restart-emacs ripgrep tfs use-package web-mode ws-butler xah-find)))
  '(read-file-name-completion-ignore-case t)
  '(recentf-mode 1)
  '(scroll-bar-mode nil)
